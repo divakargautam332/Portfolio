27b5174fb857888e3bdb16d07c429779476b6a68
Divakar Gautam <divakargautam7239@gmail.com>
2025-11-24 16:47:25 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // GitHub Pages ke liye jaruri
+  // base remove karo Render ke liye, sirf GitHub Pages me chahiye hota hai
+  // base: '/Portfolio/',
   preview: {
-    host: true, // 0.0.0.0 par expose karega
-    port: 4173, // optional, default port
-    allowedHosts: 'all', // Render ke hostname block nahi honge
+    host: '0.0.0.0',         // Render ko external network se expose karega
+    port: 4173,               // optional
+    strictPort: true,         // agar port busy ho to fail kare
+    allowedHosts: ['portfolio-2-psyl.onrender.com'] // Add exact Render hostname
   }
 })
+
 
