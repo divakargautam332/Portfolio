3f41db32639b1b9e2a7f06c272d69281034aa227
Divakar Gautam <divakargautam7239@gmail.com>
2025-11-24 16:37:53 +0530

    add
    Set base path for GitHub Pages and configure preview options.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // GitHub Pages ke liye jaruri
+  preview: {
+    host: true, // 0.0.0.0 par expose karega
+    port: 4173, // optional, default port
+    allowedHosts: 'all', // Render ke hostname block nahi honge
+  }
 })
+
